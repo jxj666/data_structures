7479ba4d491638c2a7c471141898bbb946532bd8
jxj322991 <228322991@qq.com>
2019-03-26 12:43:00 +0800

    fix

--- 0324/bc011.js
+++ 0324/bc011.js
@@ -4,13 +4,14 @@
  * @Email: jinxiaojian@youxin.com
  * @LastEditors: 靳肖健
  * @Date: 2019-03-24 23:55:33
- * @LastEditTime: 2019-03-25 00:48:07
+ * @LastEditTime: 2019-03-25 13:06:44
  */
 function math01(number, arr) {
   var max = 0;
   var sum = 0;
   for (var i = 0; i < number; i++) {
     sum += arr[i];
+    // 和大于最大值,最大值赋值;和小于0,抛弃重置
     if (sum > max) {
       max = sum;
     } else if (sum < 0) {
